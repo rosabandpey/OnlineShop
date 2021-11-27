4ac3b37f313897eacc6d0d9dbd0cb93145fadb1a
rosabandpey <mahnazbandpey@gmail.com>
2021-11-27 13:43:21 +0330

    Css-BootStrap

---
+++ js/main.js
@@ -0,0 +1,27 @@
+/*
+<div class="product col-lg-4 col-md-6 mb-4">
+		<div class="card h-100">
+			<a href="#"
+				><img
+					class="card-img-top"
+					src="ادرس عکس محصول"
+					alt="عنوان محصول"
+			/></a>
+			<div class="card-body">
+				<h4 class="card-title">
+				    عنوان محصول
+				</h4>
+				<h5 class="product-price">قیمت محصول تومان</h5>
+				<p class="card-text">
+				    توضیحات محصول
+				</p>
+			</div>
+			<div class="card-footer">
+				<button class="btn btn-light add-to-cart" data-product-id="ایدی محصول">
+					افزودن به سبد خرید
+				</button>
+			</div>
+		</div>
+	</div>
+*/
+
