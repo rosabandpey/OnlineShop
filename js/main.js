7f94fbc24e5611e8659a1a7d505a320a3d30a964
rosabandpey <mahnazbandpey@gmail.com>
2021-11-27 17:05:53 +0330

    render-Products

--- js/main.js
+++ js/main.js
@@ -1,27 +1,53 @@
-/*
-<div class="product col-lg-4 col-md-6 mb-4">
-		<div class="card h-100">
-			<a href="#"
-				><img
-					class="card-img-top"
-					src="ادرس عکس محصول"
-					alt="عنوان محصول"
-			/></a>
-			<div class="card-body">
-				<h4 class="card-title">
-				    عنوان محصول
-				</h4>
-				<h5 class="product-price">قیمت محصول تومان</h5>
-				<p class="card-text">
-				    توضیحات محصول
-				</p>
+
+const p=fetch('http://localhost:3000/products');
+const product=$("#product-list");
+
+p.then((response)=>{
+	if(response.ok){
+		return response.json();
+	}else{
+		throw Error(response.status);
+	}
+})
+.then(data => {console.log(data)
+	renderProduct(data);
+}).catch((error) => console.log(error));
+
+
+
+
+function renderProduct(myArray){
+	const questionHtml=myArray.map(json=>{
+		
+		return `
+		<div class="product col-lg-4 col-md-6 mb-4">
+				<div class="card h-100">
+					<a href="#"
+						><img
+							class="card-img-top"
+							src=${json.image}
+							alt=${json.title}
+					/></a>
+					<div class="card-body">
+						<h4 class="card-title">
+						${json.title}
+						</h4>
+						<h5 class="product-price"> ${ new Intl.NumberFormat( { maximumSignificantDigits: 3 }).format(json.price)}  تومان</h5>
+						<p class="card-text">
+						   ${json.description}
+						</p>
+					</div>
+					<div class="card-footer">
+						<button class="btn btn-light add-to-cart" data-product-id=${json.id}>
+							افزودن به سبد خرید
+						</button>
+					</div>
+				</div>
 			</div>
-			<div class="card-footer">
-				<button class="btn btn-light add-to-cart" data-product-id="ایدی محصول">
-					افزودن به سبد خرید
-				</button>
-			</div>
-		</div>
-	</div>
-*/
+		`;
+		
+	});
+
+	product.html(questionHtml.join(""));
+}
 
